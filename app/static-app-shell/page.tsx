07d1fbb80ca31c3985cdf6f2d2abf6854c9afa6f
Konstantinos Psychogios <KPsychogios@icloud.com>
2026-03-01 23:21:44 +0200

    added loading spinner in /static-app-shell

--- app/static-app-shell/page.tsx
+++ app/static-app-shell/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { Spinner } from "@/frontend/components/ui/spinner";
 
-const App = dynamic(() => import("@/frontend/app"), { ssr: false });
+const App = dynamic(() => import("@/frontend/app"), {
+  loading: () => (
+    <div className="flex items-center justify-center min-h-screen">
+      <Spinner />
+    </div>
+  ),
+  ssr: false,
+});
 
 export default function Home() {
   return <App />;
